e5b9c3c14f3be3253f17353dc6165426e685e97f
Voki <Voki@DESKTOP-VT882NJ>
2016-05-31 18:48:14 +0200

    Created TSP. Adjazenzmatrix created.

---
+++ src/tsp/Adjazenzmatrix.java
@@ -0,0 +1,21 @@
+package tsp;
+
+
+import java.io.File;
+import java.util.Vector;
+
+/**
+ * @author Voki
+ */
+public class Adjazenzmatrix {
+    
+    
+    
+
+    public Adjazenzmatrix(File numbers) {
+        
+        
+    }
+    
+
+}
---
+++ src/tsp/Main.java
@@ -0,0 +1,15 @@
+package tsp;
+
+import java.io.File;
+
+/**
+ * @author Voki
+ */
+public class Main {
+    public static void main(String[] args) {
+      File numbers = new File("numbers.txt");
+      Adjazenzmatrix matrix = new Adjazenzmatrix(numbers);
+      
+        
+    }
+}
