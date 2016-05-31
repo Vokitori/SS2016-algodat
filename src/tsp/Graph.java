43c39f25b72af66b7d1a7c079528ed8140e98ae8
link <link@fresh-linkslap>
2016-05-31 20:17:18 +0200

    Graph now can generate all edges from nodes.

--- src/tsp/Graph.java
+++ src/tsp/Graph.java
@@ -1,6 +1,5 @@
 package tsp;
 
-
 import java.io.File;
 import java.util.Vector;
 
@@ -8,15 +7,28 @@ import java.util.Vector;
  * @author Voki
  */
 public class Graph {
-    
+
     private Node nodes[];
-    
+
     private double adjazenzmatrix[][];
 
     public Graph(File numbers) {
-        
         nodes = new Node[5];
     }
-    
 
+    private void generateAdjazenzmatrix() {
+        adjazenzmatrix = new double[nodes.length][nodes.length];
+        for (int x = 0; x < nodes.length; x++) {
+            for (int y = 0; y <= x; y++) {
+                adjazenzmatrix[x][y]
+                        = (adjazenzmatrix[y][x] = distance(nodes[x], nodes[y]));             
+            }
+        }
+    }
+
+    public static final double distance(Node n, Node m) {
+        double Δx = n.x - m.x;
+        double Δy = n.y - m.y;
+        return Math.sqrt(Δx * Δx + Δy * Δy);
+    }
 }
